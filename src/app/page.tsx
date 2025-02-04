4f1d05f43938ae2c72f1985e4d5fe33194ac962a
Mehmet <mehmetext68@gmail.com>
2025-02-04 09:24:50 +0300

    feat: add notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,20 @@ import ServerSideFns from "./components/server-side-fns";
 export default function Home() {
   return (
     <div className="container my-4 flex flex-col gap-10">
+      <div className="flex flex-col gap-1">
+        <b>Notlar:</b>
+        <ul className="list-disc pl-5">
+          <li>
+            Server side fonksiyonların çalıştığı ortamda timezone offset değeri
+            0 olur. UTC timezone&apos;da çalışır.
+          </li>
+          <li>
+            Kullanıcıya gösterirken client side göstermeliyiz. Çünkü server side
+            tek bir UTC tarih alır. Fakat kullanıcılar farklı
+            timezone&apos;larda olabilir.
+          </li>
+        </ul>
+      </div>
       <ServerSideFns />
       <ClientSideFns />
       <LogFns />
