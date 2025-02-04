6d9e0a0c28686d303efdf5fe792aadcd9b7e7578
Mehmet <mehmetext68@gmail.com>
2025-02-04 09:35:06 +0300

    feat: add notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,52 @@ export default function Home() {
   return (
     <div className="container my-4 flex flex-col gap-10">
       <div className="flex flex-col gap-1">
-        <b>Notlar:</b>
+        <b>Önemli Timezone Notları:</b>
         <ul className="list-disc pl-5">
           <li>
-            Server side fonksiyonların çalıştığı ortamda timezone offset değeri
-            0 olur. UTC timezone&apos;da çalışır.
+            <b>Server Side Davranışı:</b>
+            <ul className="list-disc pl-5 mt-1">
+              <li>
+                Server tarafında çalışan fonksiyonlar her zaman UTC (Coordinated
+                Universal Time) zaman diliminde çalışır
+              </li>
+              <li>Bu nedenle timezone offset değeri her zaman 0&apos;dır</li>
+              <li>
+                Örneğin, Türkiye&apos;deki bir sunucu bile olsa, tarih işlemleri
+                UTC&apos;de gerçekleşir
+              </li>
+            </ul>
           </li>
-          <li>
-            Kullanıcıya gösterirken client side göstermeliyiz. Çünkü server side
-            tek bir UTC tarih alır. Fakat kullanıcılar farklı
-            timezone&apos;larda olabilir.
+          <li className="mt-2">
+            <b>Client Side Gerekliliği:</b>
+            <ul className="list-disc pl-5 mt-1">
+              <li>
+                Kullanıcılar dünyanın farklı yerlerinden sitemize erişebilir
+                (örn: Türkiye, Japonya, ABD)
+              </li>
+              <li>
+                Her kullanıcının kendi yerel saat dilimine göre tarihleri
+                görmesi gerekir
+              </li>
+              <li>
+                Bu yüzden tarih gösterimlerini client tarafında yapmalı ve
+                kullanıcının tarayıcısının timezone&apos;unu kullanmalıyız
+              </li>
+            </ul>
+          </li>
+          <li className="mt-2">
+            <b>Best Practice:</b>
+            <ul className="list-disc pl-5 mt-1">
+              <li>
+                Veritabanında ve API&apos;lerde tarihleri her zaman UTC olarak
+                sakla
+              </li>
+              <li>Kullanıcıya gösterirken client tarafında dönüşüm yap</li>
+              <li>
+                Tarih inputlarında da aynı şekilde önce yerel saatten
+                UTC&apos;ye çevir, sonra server&apos;a gönder
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
